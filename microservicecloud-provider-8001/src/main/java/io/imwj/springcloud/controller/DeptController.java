9fd7acd7b82c6434967816257fc5870d77e8c233
langao_q <2916863213@qq.com>
2020-04-08 18:05:25 +0800

    提交：Eureka集群搭建完善

--- microservicecloud-provider-8001/src/main/java/io/imwj/springcloud/controller/DeptController.java
+++ microservicecloud-provider-8001/src/main/java/io/imwj/springcloud/controller/DeptController.java
@@ -3,6 +3,8 @@ package io.imwj.springcloud.controller;
 import io.imwj.springcloud.entity.Dept;
 import io.imwj.springcloud.service.DeptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,6 +18,9 @@ public class DeptController {
 
     @Autowired
     private DeptService deptService;
+
+    @Autowired
+    private DiscoveryClient client;
 
     @RequestMapping(value = "/dept/add", method = RequestMethod.POST)
     public boolean add(@RequestBody Dept dept){
@@ -33,6 +38,19 @@ public class DeptController {
         return list;
     }
 
+    @RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+    public Object discovery()
+    {
+        List<String> list = client.getServices();
+        System.out.println("**********" + list);
+
+        List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+        for (ServiceInstance element : srvList) {
+            System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+                    + element.getUri());
+        }
+        return this.client;
+    }
 
 
 }
