e1efa7e9b02738454e02884db098bff391991b43
langao_q <2916863213@qq.com>
2020-04-14 14:26:53 +0800

    提交：microservice-config配置提交

---
+++ microservicecloud-config-3344/src/main/java/io/imwj/springcloud/Config_3344_StartSpringCloudApp.java
@@ -0,0 +1,18 @@
+package io.imwj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * @author langao_q
+ * @create 2020-04-14 11:12
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class Config_3344_StartSpringCloudApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Config_3344_StartSpringCloudApp.class, args);
+    }
+}
---
+++ microservicecloud-config-client-3355/src/main/java/io/imw/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,16 @@
+package io.imw.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author langao_q
+ * @create 2020-04-14 11:52
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class,args);
+    }
+}
