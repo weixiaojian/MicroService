539ed4e3e202fcade934e22cf8c327f4c8d48bcf
langao_q <2916863213@qq.com>
2020-04-14 15:23:38 +0800

    提交：microservice-config配置提交

--- microservicecloud-config-eureka-client-7001/src/main/java/io/imwj/springcloud/Config_Git_EurekaServerApplication.java
+++ microservicecloud-config-eureka-client-7001/src/main/java/io/imwj/springcloud/Config_Git_EurekaServerApplication.java
@@ -10,8 +10,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class Config_Git_EurekaServerApplication_7001 {
+public class Config_Git_EurekaServerApplication {
     public static void main(String[] args) {
-        SpringApplication.run(Config_Git_EurekaServerApplication_7001.class, args);
+        SpringApplication.run(Config_Git_EurekaServerApplication.class, args);
     }
 }
